a035e9305a414d5483c800a6239f2c4d71adcf0c
07031218 <minshunqiang@gmail.com>
2021-08-14 12:58:44 +0800

    被解锁服务器添加修改dns服务器用的定时任务

---
+++ cron.sh
@@ -0,0 +1,10 @@
+#bin/bash
+crontab -l 2>/dev/null > /root/crontab_test 
+echo '*/5 * * * * bash <(curl -L -s https://git.io/JR7RH)' >> /root/crontab_test 
+crontab /root/crontab_test 
+crontask=(crontab -l)
+echo -------------------------------------------------------
+echo "设置定时任务成功，当前系统所有定时任务清单如下:/n ${crontask}"
+echo -------------------------------------------------------
+
+exit
