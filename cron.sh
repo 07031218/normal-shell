df0ba91d7e8d39f40befe8ed96b5097037bceee5
07031218 <minshunqiang@gmail.com>
2021-08-14 13:20:15 +0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,8 +1,12 @@
 #bin/bash
+echo "开始进行解锁"
+bash <(curl -L -s https://git.io/JR7RH)
+echo "开始添加定时任务"
 crontab -l 2>/dev/null > /root/crontab_test 
 echo '*/5 * * * * bash <(curl -L -s https://git.io/JR7RH)' >> /root/crontab_test 
 crontab /root/crontab_test 
 crontask=$(crontab -l)
+
 echo -------------------------------------------------------
 echo -e "设置定时任务成功，当前系统所有定时任务清单如下:\n${crontask}"
 echo -------------------------------------------------------
