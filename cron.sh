a5a7ee612ab84582e8f95eb9e2db115d56a2c278
07031218 <minshunqiang@gmail.com>
2021-08-14 13:11:14 +0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -4,8 +4,7 @@ echo '*/5 * * * * bash <(curl -L -s https://git.io/JR7RH)' >> /root/crontab_test
 crontab /root/crontab_test 
 crontask=$(crontab -l)
 echo -------------------------------------------------------
-echo "设置定时任务成功，当前系统所有定时任务清单如下:
-      ${crontask}"
+echo -e "设置定时任务成功，当前系统所有定时任务清单如下:\n${crontask}"
 echo -------------------------------------------------------
 
 exit
