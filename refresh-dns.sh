34f0ea14665dcb6255636641d616310746d326eb
07031218 <minshunqiang@gmail.com>
2021-08-24 07:22:36 +0800

    美化显示效果

--- refresh-dns.sh
+++ refresh-dns.sh
@@ -18,10 +18,10 @@ URL=${HOST}.${DOMAIN}
 #IP=`ping ${URL} -c 1 |awk 'NR==2 {print $5}' |awk -F ':' '{print $1}' |sed -nr "s#\(##gp"|sed -nr "s#\)##gp"`
 #dig命令获取域名的ip地址
 IP=`dig ${URL} @223.5.5.5 | awk -F "[ ]+" '/IN/{print $1}' | awk 'NR==2 {print $5}'`
-echo "解锁dns服务器IP地址是 ${IP}"
+echo -e "${BLUE}解锁dns服务器IP地址是 ${IP} ${PLAIN}"
 
 
 
-echo "${YELLOW}开始修改本机DNS服务器地址${PLAIN}"
+echo -e "${YELLOW}开始修改本机DNS服务器地址${PLAIN}"
 chattr -i /etc/resolv.conf && echo -e "nameserver $IP\nnameserver 8.8.8.8" > /etc/resolv.conf && chattr +i /etc/resolv.conf
-echo "${GREEN}修改DNS服务器地址完成，开始畅游Netflix吧^_^${PLAIN}"
+echo -e "${GREEN}修改DNS服务器地址完成，开始畅游Netflix吧^_^${PLAIN}"
