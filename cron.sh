040044857c1e046e21f66022dec55df6f36ebd06
07031218 <minshunqiang@gmail.com>
2021-08-25 15:01:59 +0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -3,7 +3,7 @@ echo "开始进行解锁"
 wget -O /root/refresh-dns.sh https://git.io/JR7RH && chmod +x /root/refresh-dns.sh && bash /root/refresh-dns.sh
 echo "开始添加定时任务"
 crontab -l 2>/dev/null > /root/crontab_test 
-echo '*/5 * * * * /root/refresh-dns.sh' >> /root/crontab_test 
+echo '*/5 * * * * /usr/bin/bash /root/refresh-dns.sh' >> /root/crontab_test 
 crontab /root/crontab_test 
 crontask=$(crontab -l)
 
