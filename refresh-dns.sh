f13d21096b3f2186d9528dedef4b95395dc1a1bf
07031218 <minshunqiang@gmail.com>
2021-10-09 12:33:12 +0800

    修复nameserver被设置为空白的BUG

--- refresh-dns.sh
+++ refresh-dns.sh
@@ -13,17 +13,23 @@ yum install bind-utils -y  2> /dev/null || apt-get install dnsutils  -y
 #检测dig命令是否安装成功
 hash dig 2> /dev/null || { echo -e >&2 "${RED}出现异常 dnsutils 没有安装成功，脚本无法继续，将自动退出...${PLAIN}"; exit 1; }
 #定义变量
-HOST="nf"
-DOMAIN="20120714.xyz"
-URL=${HOST}.${DOMAIN}
+URL=nf.20120714.xyz
 #IP=`ping ${URL} -c 1 |awk 'NR==2 {print $4}' |awk -F ':' '{print $1}'`
 #IP=`ping ${URL} -c 1 |awk 'NR==2 {print $5}' |awk -F ':' '{print $1}' |sed -nr "s#\(##gp"|sed -nr "s#\)##gp"`
 #dig命令获取域名的ip地址
 IP=`dig ${URL} @223.5.5.5 | awk -F "[ ]+" '/IN/{print $1}' | awk 'NR==2 {print $5}'`
-echo -e "${BLUE}解锁dns服务器IP地址是 ${IP} ${PLAIN}"
 
-
-
-echo -e "${YELLOW}开始修改本机DNS服务器地址${PLAIN}"
-chattr -i /etc/resolv.conf && echo -e "nameserver ${IP}\nnameserver 8.8.8.8" > /etc/resolv.conf && chattr +i /etc/resolv.conf
+echo -e "${BLUE}当前设定的解锁dns服务器IP地址是 ${IP} ${PLAIN}"
+record=$(sed -n '1p' /etc/resolv.conf | grep 'nameserver'| sed 's/^.*nameserver//g' | sed 's/\"//g' | sed 's/\,//g' | sed 's/ //g')
+        if [[ -n "${record}" ]] && [["${IP}" = "${record}"]]; then
+                echo -e "${GREEN}-----------------------------------------------------------------------------${PLAIN}"
+                echo -e "${GREEN}DNS解锁服务器地址无变化，无需修改, current IP: ${IP}${PLAIN}"
+                echo -e "${GREEN}-----------------------------------------------------------------------------${PLAIN}"
+                exit
+        else
+echo -e "${YELLOW}解锁服务器IP有变化，开始修改本机DNS服务器地址${PLAIN}"
+chattr -i /etc/resolv.conf && echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" > /etc/resolv.conf && chattr +i /etc/resolv.conf 
+IP1=`dig ${URL} @223.5.5.5 | awk -F "[ ]+" '/IN/{print $1}' | awk 'NR==2 {print $5}'`
+chattr -i /etc/resolv.conf && echo -e "nameserver ${IP1}\nnameserver 8.8.8.8" > /etc/resolv.conf && chattr +i /etc/resolv.conf
 echo -e "${GREEN}修改DNS服务器地址完成，开始畅游Netflix吧^_^ ${PLAIN}"
+fi
