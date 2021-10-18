c7275e59e6b987eac6b1ced8c81185d099ab2e8c
07031218 <minshunqiang@gmail.com>
2021-10-18 20:03:17 +0800

    修复nameserver被设置为空白的BUG

--- refresh-dns.sh
+++ refresh-dns.sh
@@ -9,7 +9,7 @@ PURPLE="\033[0;35m"
 CYAN='\033[0;36m'
 PLAIN='\033[0m'
 #安装dig命令
-yum install bind-utils -y  2> /dev/null || apt-get install dnsutils  -y 
+yum install bind-utils -y > /dev/null 2>&1 || apt-get install dnsutils  -y > /dev/null 2>&1
 #检测dig命令是否安装成功
 hash dig 2> /dev/null || { echo -e >&2 "${RED}出现异常 dnsutils 没有安装成功，脚本无法继续，将自动退出...${PLAIN}"; exit 1; }
 #定义变量
