b28959e11d58e6c379eca4751204283b175df67e
07031218 <minshunqiang@gmail.com>
2021-10-28 16:41:44 +0800

    Update netflix-verify.sh

--- netflix-verify.sh
+++ netflix-verify.sh
@@ -17,8 +17,8 @@ checkCPU(){
 version=$(curl --silent "https://github.com/sjlleo/netflix-verify/releases/latest" | sed 's#.*tag/\(.*\)".*#\1#')
 checkCPU
 #下载检测程序
-wget -O /root/nf https://github.com/sjlleo/netflix-verify/releases/download/${version}/nf_${version}_${arch} > /dev/null 2>&1 
-chmod +x /root/nf > /dev/null 2>&1 
-/root/nf -method full
-rm /root/nf
+wget -O nf https://github.com/sjlleo/netflix-verify/releases/download/${version}/nf_${version}_${arch} > /dev/null 2>&1 
+chmod +x nf > /dev/null 2>&1 
+./nf -method full
+rm nf
 exit
