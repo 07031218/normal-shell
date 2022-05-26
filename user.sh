a21424eeb25427508ecbf5e7687cf522f57e3640
07031218 <minshunqiang@gmail.com>
2022-05-26 16:53:24 +0800

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+#=================================================================#
+#   System Required: CentOS 6+, Debian8+, Ubuntu16+               #
+#   Description: One click adduser and deluser for linux          #
+#   Author: 翔翎 <https://blog.20120714.xyz>                       #
+#=================================================================#
+
 echoType='echo -e'
 echoContent() {
   case $1 in
