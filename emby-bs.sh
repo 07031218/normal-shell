4b924e48f6b33ad52a683cc0dcc372299c1a87ec
07031218 <minshunqiang@gmail.com>
2022-09-21 16:45:33 +0800

    Create emby-bs.sh

--- emby-bs.sh
+++ emby-bs.sh
@@ -105,7 +105,8 @@ backup_emby(){
     fi
     cd $config_dir
     echoContent yellow "Emby-VarLibEmby数据库备份中，请耐心等待······"
-    targz ${backto_dir}/${DATE}/Emby-VarLibEmby数据库.tar.gz ./emby/
+    # 备份VarLibEmby数据库(排除包含帐户数据相关的文件)
+    targz ${backto_dir}/${DATE}/Emby-VarLibEmby数据库.tar.gz ./emby/ --exclude ./emby/data/device.txt --exclude ./emby/data/users.db --exclude ./emby/data/activitylog.db --exclude ./emby/data/authentication.db --exclude ./emby/data/displaypreferences.db
     if [[ "$?" -eq 0 ]]; then
         # clear
         echoContent green "Emby-VarLibEmby数据库备份完成"
