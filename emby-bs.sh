e4848bf10c4b213e045ccfa46d7f28a5b9427915
07031218 <minshunqiang@gmail.com>
2022-10-12 12:47:52 +0800

    Update emby-bs.sh

--- emby-bs.sh
+++ emby-bs.sh
@@ -69,13 +69,43 @@ backup_emby(){
     echoContent skyBlue "请输入备份文件的存放路径⬇"
     read backto_dir
     mkdir -p $backto_dir/$DATE
-    echoContent skyBlue "请输入Emby削刮库的目录路径，留空则默认为/var/lib/emby/programdata/"
+    echoContent skyBlue "请输入Emby削刮库的目录路径，如未自定义过削刮缓存目录或者你看不懂这条在说什么，那么直接回车即可⬇"
     read xuegua_dir
+    echoContent skyBlue "是否需要备份Emby-server主程序[Y/N]:"
+    read baksys
     echoContent white "即将开始对Emby Server进行备份，需要一定的时间，请耐心等待······"
     sleep 3s
     if [[ ${xuegua_dir} == "" ]]; then
-        xuegua_dir=/var/lib/emby/programdata/
+        xuegua_dir=/var/lib/emby/
         systemctl stop emby-server
+        cd $xuegua_dir
+        echoContent yellow "Emby削刮包和数据库备份中，请耐心等待······"
+        targz ${backto_dir}/${DATE}/Emby削刮包和LibEmby数据库.tar.gz ./
+        if [[ "$?" -eq 0 ]]; then
+                # clear
+            echoContent green "Emby削刮包和LibEmby数据库备份完成"
+            sleep 5s
+        else
+            echoContent red "Emby削刮包和LibEmby数据库备份失败"
+            systemctl start emby-server
+            exit 1
+        fi
+        if [[ $baksys == "Y" ]]||[[ $baksys == "y" ]]; then
+            cd $sys_dir
+            echoContent yellow "Emby-server主程序备份中，请耐心等待······"
+            targz ${backto_dir}/${DATE}/Emby-server数据库.tar.gz ./
+            if [[ "$?" -eq 0 ]]; then
+                # clear
+                echoContent green "Emby-server主程序备份完成"
+                sleep 5s
+            else
+                echoContent red "Emby-server主程序备份失败"
+                systemctl start emby-server
+                exit 1
+            fi
+        fi
+        echoContent yellow "恭喜，所有备份均已完成。"
+        systemctl start emby-server
     else
         systemctl stop emby-server
         cd $xuegua_dir
@@ -90,33 +120,35 @@ backup_emby(){
             systemctl start emby-server
             exit 1
         fi
-    fi
-    cd $sys_dir
-    echoContent yellow "Emby-server数据库备份中，请耐心等待······"
-    targz ${backto_dir}/${DATE}/Emby-server数据库.tar.gz ./
-    if [[ "$?" -eq 0 ]]; then
-        # clear
-        echoContent green "Emby-server数据库备份完成"
-        sleep 5s
-    else
-        echoContent red "Emby-server数据库备份失败"
+        cd $config_dir
+        echoContent yellow "LibEmby数据库备份中，请耐心等待······"
+        # 备份VarLibEmby数据库(排除包含帐户数据相关的文件)
+        targz ${backto_dir}/${DATE}/LibEmby数据库.tar.gz ./emby/ '--exclude ./emby/data/device.txt --exclude ./emby/data/users.db --exclude ./emby/data/activitylog.db --exclude ./emby/data/authentication.db --exclude ./emby/data/displaypreferences.db'
+        if [[ "$?" -eq 0 ]]; then
+            # clear
+            echoContent green "LibEmby数据库备份完成"
+        else
+            echoContent red "LibEmby数据库备份失败"
+            systemctl start emby-server
+            exit 1
+        fi
+        if [[ $baksys == "Y" ]]||[[ $baksys == "y" ]]; then
+            cd $sys_dir
+            echoContent yellow "Emby-server主程序备份中，请耐心等待······"
+            targz ${backto_dir}/${DATE}/Emby-server数据库.tar.gz ./
+            if [[ "$?" -eq 0 ]]; then
+                # clear
+                echoContent green "Emby-server主程序备份完成"
+                sleep 5s
+            else
+                echoContent red "Emby-server主程序备份失败"
+                systemctl start emby-server
+                exit 1
+            fi
+        fi
+        echoContent yellow "恭喜，所有备份均已完成。"
         systemctl start emby-server
-        exit 1
     fi
-    cd $config_dir
-    echoContent yellow "Emby-VarLibEmby数据库备份中，请耐心等待······"
-    # 备份VarLibEmby数据库(排除包含帐户数据相关的文件)
-    targz ${backto_dir}/${DATE}/Emby-VarLibEmby数据库.tar.gz ./emby/ '--exclude ./emby/data/device.txt --exclude ./emby/data/users.db --exclude ./emby/data/activitylog.db --exclude ./emby/data/authentication.db --exclude ./emby/data/displaypreferences.db'
-    if [[ "$?" -eq 0 ]]; then
-        # clear
-        echoContent green "Emby-VarLibEmby数据库备份完成"
-    else
-        echoContent red "Emby-VarLibEmby数据库备份失败"
-        systemctl start emby-server
-        exit 1
-    fi
-    echoContent yellow "恭喜，所有备份均已完成。"
-    systemctl start emby-server
 }
 restore_emby(){
     echoContent skyBlue "请输入Emby削刮库的目录路径，留空则默认为/var/lib/emby/programdata/"
