3af4a087e1676b0687b67d8ec4e9661b23a51bf9
07031218 <minshunqiang@gmail.com>
2022-10-12 13:10:37 +0800

    Update back-emby.sh

--- back-emby.sh
+++ back-emby.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
+xuegua_dir="" # 削刮库目录,如未自定义，留空即可
+embylib_dir="/var/lib/emby" # Emby默认Libray数据库和削刮包缓存目录,不要修改
+embyserver_dir="/opt/emby-server/" # Emby主程序相关目录，如果不需要备份主程序，删除内容留空即可
+bak_dir="/nongjiale/emby-server-bak" # 备份文件存放目录
+DATE=`date +%Y%m%d` # 时间格式化，勿动
 
-# 削刮库目录
-xuegua_dir="/home/Emby"
-# Emby数据库等配置目录,默认不要修改
-embylib_dir="/var/lib/"
-# Emby主程序相关目录
-embyserver_dir="/opt/emby-server/"
-# 备份文件存放目录
-bak_dir="/nongjiale/emby-server-bak"
-# 时间格式化，如 20220602
-DATE=`date +%Y%m%d`
-# 备份脚本保存的天数
-DEL_DAY=7
+DEL_DAY=7 # 备份脚本保存的天数，默认7天，支持自定义修改
 targz(){
     if [[ `which pv` == "" ]]; then
         apt install pv -y || yum install pv -y
@@ -20,7 +14,6 @@ targz(){
         echo "参数缺失，用法 'targz 压缩名 文件名/目录名'"
         exit 1
     fi
-
     tar -cf - $3 $2  | pv -s $(du -sk $2 | awk '{print $1}') | gzip > $1
 }
 # 创建日期目录
@@ -30,18 +23,46 @@ systemctl stop emby-server
 if [[ $xuegua_dir != "" ]]; then
     cd $xuegua_dir
     targz $bak_dir/$DATE/Emby削刮包.tar.gz ./
-    echo "Emby削刮包备份完成······"
+    if [[ $? -eq 0 ]]; then
+        echo "Emby削刮包备份完成······"
+    else
+        echo "`DATE` Emby削刮包备份失败" >> /root/emby_bak_error.log
+        systemctl start emby-server
+        exit 1
+    fi
+    cd $embylib_dir
+    # 备份VarLibEmby数据库(排除包含帐户数据相关的文件)
+    targz $bak_dir/${DATE}/Emby-VarLibEmby数据库.tar.gz ./emby '--exclude ./emby/data/device.txt --exclude ./emby/data/users.db --exclude ./emby/data/activitylog.db --exclude ./emby/data/authentication.db --exclude ./emby/data/displaypreferences.db'
+    if [[ $? -eq 0 ]]; then
+        echo "LibEmby数据库备份完成······"
+    else
+        echo "`DATE` LibEmby数据库备份失败" >> /root/emby_bak_error.log
+        systemctl start emby-server
+        exit 1
+    fi
+else
+    cd $xuegua_dir
+    targz $bak_dir/$DATE/Emby削刮包和LibEmby数据库.tar.gz ./
+    if [[ $? -eq 0 ]]; then
+        echo "Emby削刮包和LibEmby数据库备份完成······"
+    else
+        echo "`DATE` Emby削刮包和LibEmby数据库备份失败" >> /root/emby_bak_error.log
+        systemctl start emby-server
+        exit 1
+    fi
 fi
-cd $embyserver_dir
-targz $bak_dir/${DATE}/Emby-server数据库.tar.gz ./
-echo "Emby-server数据库备份完成······"
-cd $embylib_dir
-# 备份VarLibEmby数据库(排除包含帐户数据相关的文件)
-targz $bak_dir/${DATE}/Emby-VarLibEmby数据库.tar.gz ./emby '--exclude ./emby/data/device.txt --exclude ./emby/data/users.db --exclude ./emby/data/activitylog.db --exclude ./emby/data/authentication.db --exclude ./emby/data/displaypreferences.db'
-echo "Emby-VarLibEmby数据库备份完成······"
-# 启动Emby Server服务
-systemctl start emby-server
-
+if [[ $embyserver_dir != "" ]]; then
+    cd $embyserver_dir
+    targz $bak_dir/${DATE}/Emby-server主程序.tar.gz ./
+    if [[ $? -eq 0 ]]; then
+        echo "Emby-server主程序备份完成······"
+    else
+        echo "`DATE` Emby-server主程序备份失败" >> /root/emby_bak_error.log
+        systemctl start emby-server
+        exit 1
+    fi
+fi
+systemctl start emby-server # 启动Emby Server服务
 # 遍历备份目录下的日期目录
 LIST=$(ls $bak_dir)
 # 获取7天前的时间，用于作比较，早于该时间的文件将删除
