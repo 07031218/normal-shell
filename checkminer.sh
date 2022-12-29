606ddbc968be8fc79ebbed623b4842d1e1787b21
07031218 <minshunqiang@gmail.com>
2022-12-29 13:43:29 +0800

    Update checkminer.sh

--- checkminer.sh
+++ checkminer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cat > /usr/bin/tongzhi <<EOF
 #!/bin/bash
-baseip=\$(curl -4 ipip.ooo)
+baseip=\$(curl -4 ip.sb)
 if [[ -d /tmp/xmr ]]||[[ -f /tmp/config.json ]]||[[ -f /usr/.work/work32 ]]||[[ -f /usr/.work/work64 ]]; then
   # 下方对照修改填写自己tgbot的apitoken和自己的TGID即可
 	curl -s "https://api.telegram.org/bot{apitoken}/sendMessage?chat_id={tgid}&text=主人，本机发现挖矿程序，我的IP地址为：\${baseip}"
