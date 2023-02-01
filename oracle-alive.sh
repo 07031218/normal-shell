899667222676c86721188f8c843551cce7b9ce16
07031218 <minshunqiang@gmail.com>
2023-02-01 17:49:57 +0800

    Update oracle-alive.sh

--- oracle-alive.sh
+++ oracle-alive.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+if [[ `ps aux|grep cpu.py|wc -l` == 2 ]]; then
+	echo "检测到机器上已经部署过保号脚本了，程序退出。"
+	exit 0
+fi
+if [[ `which python3` == "" ]]; then
+	apt update && apt install python3 -y
+fi
 echo -ne "请输入CPU线程数："
 read cpunumber
 cpup=$(expr ${cpunumber} \* 15)
