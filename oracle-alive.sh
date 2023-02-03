a020d2259321f554ed4a3f4df89f89cf181b3207
07031218 <minshunqiang@gmail.com>
2023-02-03 10:42:28 +0800

    Update oracle-alive.sh

--- oracle-alive.sh
+++ oracle-alive.sh
@@ -3,40 +3,52 @@ if [[ $EUID -ne 0 ]]; then
 	echo -e "脚本必须root账号运行，请切换root用户后再执行本脚本!"
 	exit 1
 fi
-
 if [[ `which python3` == "" ]]; then
 	apt update || yum update
 	apt install python3 -y || yum install python3 -y 
 fi
+cpunumber=$(cat /proc/cpuinfo| grep "processor"| wc -l) # 取CPU线程数量
+cpup=$(expr ${cpunumber} \* 15)  # 设定CPU占用百分数值
+# 取内存占用数值开始处
+if [[ `uname -m` == "aarch64" ]]; then
+	memorylimit="${cpunumber}*0.6*1024*1024*1024"
+elif [[ `uname -m` == "x86_64" ]]; then
+	memorylimit="${cpunumber}*0.1*1024*1024*1024"
+fi
+# 取内存占用数值结束处
+
 if [[ -f /tmp/cpu.py ]]; then
 	systemctl stop KeepCPU
 	systemctl disable KeepCPU
-	rm /tmp/cpu.py
-elif [[ -f /etc/systemd/system/KeepCPU.service ]] && [[ `ps aux|grep cpu.py|wc -l` != 2 ]]; then
+	rm /tmp/cpu.py && rm /etc/systemd/system/KeepCPU.service
+elif [[ -f /etc/systemd/system/KeepCPU.service ]] && [[ -f /root/cpu.py ]]; then
 	systemctl stop KeepCPU
 	systemctl disable KeepCPU
-elif [[ `ps aux|grep cpu.py|wc -l` == 2 ]]; then
+	rm /root/cpu.py && rm /etc/systemd/system/KeepCPU.service
+elif [[ `ps aux|grep cpumemory.py|wc -l` == 2 ]] && [[ -f /roo/cpumemory.py ]]; then
 	echo "检测到机器上已经部署过保号脚本了，程序退出。"
 	exit 0
 fi
-cpunumber=$(cat /proc/cpuinfo| grep "processor"| wc -l)
-cpup=$(expr ${cpunumber} \* 15)
-cat > /etc/systemd/system/KeepCPU.service <<EOF
+# 配置CPU占用开始
+cat > /etc/systemd/system/KeepCpuMemory.service <<EOF
 [Unit]
 
 [Service]
 CPUQuota=${cpup}%
-ExecStart=/usr/bin/python3 /root/cpu.py
+ExecStart=/usr/bin/python3 /root/cpumemory.py
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-cat > /root/cpu.py <<EOF
+cat > /root/cpumemory.py <<EOF
+import platform
+memory = bytearray(int(${memorylimit}))
 while True:
-  x=1
+	pass
 EOF
 systemctl daemon-reload
-systemctl enable KeepCPU
-systemctl start KeepCPU
-echo "设置CPU占用保号完成。"
+systemctl start KeepCpuMemory
+systemctl enable KeepCpuMemory
+echo "设置CPU、内存占用保号完成。"
+# 配置CPU、内存占用结束
