86f9e7fd2e847e147bc5f3d6733d682da1cb7422
07031218 <minshunqiang@gmail.com>
2023-02-11 00:04:28 +0800

    Update ipip.sh

--- ipip.sh
+++ ipip.sh
@@ -21,9 +21,9 @@ else
 fi
 echo "${remoteip}" >/root/.tunnel-ip.txt
 ip tunnel add tun0 mode ipip remote ${remoteip} ttl 255 # 创建IP隧道
-ip addr add ${vip}/32 dev tun0 # 添加本机VIP
+ip addr add ${vip}/30 dev tun0 # 添加本机VIP
 ip link set tun0 up # 启用隧道虚拟网卡
-ip route add ${remotevip}/32 dev tun0 scope link src ${vip}  # 添加对端VIP走隧道的路由表
+ip route add ${remotevip}/30 dev tun0 scope link src ${vip}  # 添加对端VIP走隧道的路由表
 i=0
 read -p "请输入要添加的路由表总数量(需要对接几个vps就填写几)：" times
 while [[ $i -lt $times ]]; do
@@ -50,9 +50,9 @@ routelist=\$(ip route list|sed "\$d")
 if [[ \$oldip != \$remoteip ]]; then
 	ip tunnel del tun0 >/dev/null &
 	ip tunnel add tun0 mode ipip remote \${remoteip} ttl 255
-	ip addr add ${vip}/32 dev tun0
+	ip addr add ${vip}/30 dev tun0
 	ip link set tun0 up
-	ip route add \${remotevip}/32 dev tun0 scope link src ${vip}
+	ip route add \${remotevip}/30 dev tun0 scope link src ${vip}
 	\$routelist
 fi
 EOF
