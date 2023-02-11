60de91dd4f7e2bb48e57e28ab91621d35daf1563
07031218 <minshunqiang@gmail.com>
2023-02-11 12:54:59 +0800

    Update ipip.sh

--- ipip.sh
+++ ipip.sh
@@ -20,25 +20,16 @@ else
 	remoteip=$(dig ${ddnsname} @8.8.8.8 | awk -F "[ ]+" '/IN/{print $1}' | awk 'NR==2 {print $5}')
 fi
 echo "${remoteip}" >/root/.tunnel-ip.txt
-ip tunnel add tun0 mode ipip remote ${remoteip} ttl 255 # 创建IP隧道
+ip tunnel add tun0 mode ipip remote ${remoteip} local $(ip a 2>&1 | grep -w 'inet' | grep 'global' | grep -E '\ 1(92|0|72|00|1)\.' | sed 's/.*inet.//g' | sed 's/\/[0-9][0-9].*$//g' | head -n 1) ttl 64 # 创建IP隧道
 ip addr add ${vip}/30 dev tun0 # 添加本机VIP
 ip link set tun0 up # 启用隧道虚拟网卡
-ip route add ${remotevip}/30 dev tun0 scope link src ${vip}  # 添加对端VIP走隧道的路由表
-i=0
-read -p "请输入要添加的路由表总数量(需要对接几个vps就填写几)：" times
-while [[ $i -lt $times ]]; do
-	let i++
-	read -p "请添加第${i}个vps的IP地址：" vpsip
-	ip route add ${vpsip} dev tun0 scope link src ${vip} # 
-done
-read -p "本机是否跳板机(跳板机需要开启NAT转发)[Y/n]:" yn
-if [[ $yn == "Y" ]]||[[ $yn == "y" ]]; then
-	iptables -t nat -A POSTROUTING -s $remotevip -j MASQUERADE
+ip route add ${remotevip}/32 dev tun0 scope link src ${vip}
+if [[ `iptables -t nat -L|grep "${remotevip}"` == "" ]]; then
+	iptables -t nat -A POSTROUTING -s ${remotevip} -j MASQUERADE
 fi
-echo "IPIP隧道和相关路由表已经配置完毕，开始添加自动修改对端IP定时任务脚本。"
-
-if [[ $yn != "Y" ]] && [[ $yn != "y" ]]; then
-	cat >/root/change-tunnel-ip.sh <<EOF
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
+cat >/root/change-tunnel-ip_${ddnsname}.sh <<EOF
 #!/bin/bash
 if [[ \`dig ${ddnsname} @8.8.8.8| grep 'ANSWER SECTION'\` == "" ]]; then
 	remoteip="${ddnsname}"
@@ -49,24 +40,22 @@ oldip="\$(cat /root/.tunnel-ip.txt)"
 routelist=\$(ip route list|sed "\$d")
 if [[ \$oldip != \$remoteip ]]; then
 	ip tunnel del tun0 >/dev/null &
-	ip tunnel add tun0 mode ipip remote \${remoteip} ttl 255
+	ip tunnel add tun0 mode ipip remote \${remoteip} local \$(ip a 2>&1 | grep -w 'inet' | grep 'global' | grep -E '\ 1(92|0|72|00|1)\.' | sed 's/.*inet.//g' | sed 's/\/[0-9][0-9].*$//g' | head -n 1) ttl 64
 	ip addr add ${vip}/30 dev tun0
 	ip link set tun0 up
-	ip route add \${remotevip}/30 dev tun0 scope link src ${vip}
-	\$routelist
+	ip route add \${remotevip}/32 dev tun0 scope link src ${vip}
 fi
 EOF
-	echo "开始添加定时任务"
-	bashsrc=$(which bash)
-	crontab -l 2>/dev/null > /root/crontab_test 
-	echo -e "*/2 * * * * ${bashsrc} /root/change-tunnel-ip.sh" >> /root/crontab_test 
-	crontab /root/crontab_test 
-	rm /root/crontab_test
-	crontask=$(crontab -l)
+echo "开始添加定时任务"
+bashsrc=$(which bash)
+crontab -l 2>/dev/null > /root/crontab_test 
+echo -e "*/2 * * * * ${bashsrc} /root/change-tunnel-ip_${ddnsname}.sh" >> /root/crontab_test 
+crontab /root/crontab_test 
+rm /root/crontab_test
+crontask=$(crontab -l)
 
-	echo -------------------------------------------------------
-	echo -e "设置定时任务成功，当前系统所有定时任务清单如下:\n${crontask}"
-	echo -------------------------------------------------------
-	echo "程序全部执行完毕，脚本退出。。"
-	exit 0
-fi
+echo -------------------------------------------------------
+echo -e "设置定时任务成功，当前系统所有定时任务清单如下:\n${crontask}"
+echo -------------------------------------------------------
+echo "程序全部执行完毕，脚本退出。。"
+exit 0
