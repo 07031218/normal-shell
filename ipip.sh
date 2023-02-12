c26958a6a7ab89a3196f8c2e5eb7d285044b5be5
07031218 <minshunqiang@gmail.com>
2023-02-12 23:38:35 +0800

    Update ipip.sh

--- ipip.sh
+++ ipip.sh
@@ -24,38 +24,12 @@ install_ipip(){
 	read vip
 	echo -ne "请输入对端的V-IP："
 	read remotevip
-	if [[ `ping $ddnsname -c 1|awk 'NR==2 {print $5}' |awk -F ':' '{print $1}' |sed -nr "s#\(##gp"|sed -nr "s#\)##gp"` ==  $ddnsname ]]; then
+	localip=$(ip a |grep brd|grep global|awk '{print $2}'|grep /24|awk -F "/" '{print $1}')
+	if [[ `ping $ddnsname -c 1|awk "NR==2 {print $5}" |awk -F ':' "{print $1}" |awk '{print $4}'` ==  $ddnsname ]]; then
 		remoteip=${ddnsname}
 	else
-		remoteip=$(ping $ddnsname -c 1|awk 'NR==2 {print $5}' |awk -F ':' '{print $1}' |sed -nr "s#\(##gp"|sed -nr "s#\)##gp")
-			cat >/root/change-tunnel-ip_${ddnsname}.sh <<EOF
-#!/bin/bash
-remoteip=\$(ping $ddnsname -c 1|awk "NR==2 {print \$5}" |awk -F ':' "{print \$1}" |sed -nr "s#\(##gp"|sed -nr "s#\)##gp")
-oldip="\$(cat /root/.tunnel-ip.txt)"
-localip=$(ip a |grep brd|grep global|awk '{print $2}'|grep /24|awk -F "/" '{print $1}')
-if [[ \$oldip != \$remoteip ]]; then
-	ip tunnel del $tunname >/dev/null &
-	ip tunnel add $tunname mode ipip remote \${remoteip} local \${localip} ttl 64
-	ip addr add ${vip}/30 dev $tunname
-	ip link set $tunname up
-	sed -i "s/ip tunnel add $tunname mode ipip remote \${oldip} local ${localip} ttl 64/ip tunnel add $tunname mode ipip remote \${remoteip} local ${localip} ttl 64/g" /etc/rc.local
-fi
-EOF
-	fi
-	localip=$(ip a |grep brd|grep global|awk '{print $2}'|grep /24|awk -F "/" '{print $1}')
-	echo "${remoteip}" >/root/.tunnel-ip.txt
-	ip tunnel add $tunname mode ipip remote ${remoteip} local $localip ttl 64 # 创建IP隧道
-	ip addr add ${vip}/30 dev $tunname # 添加本机VIP
-	ip link set $tunname up # 启用隧道虚拟网卡
-	ip route add ${remotevip}/32 dev $tunname scope link src ${vip}
-	if [[ `iptables -t nat -L|grep "${remotevip}"` == "" ]]; then
-		iptables -t nat -A POSTROUTING -s ${remotevip} -j MASQUERADE
-	fi
-	if [[ `sysctl -p|grep "net.ipv4.ip_forward = 1"` == "" ]]; then
-		echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
-		sysctl -p /etc/sysctl.conf
-	fi
-cat > /etc/rc.local <<EOF
+		remoteip=$(ping $ddnsname -c 1|awk "NR==2 {print $5}" |awk -F ':' "{print $1}" |awk '{print $4}')
+		cat > /etc/rc.local <<EOF
 #!/bin/sh -e
 #
 # rc.local
@@ -75,6 +49,33 @@ ip addr add ${vip}/30 dev $tunname
 ip link set $tunname up
 exit 0
 EOF
+		cat >/root/change-tunnel-ip_${ddnsname}.sh <<EOF
+#!/bin/bash
+remoteip=\$(ping $ddnsname -c 1|awk "NR==2 {print \$5}" |awk -F ':' "{print \$1}" |awk '{print \$4}')
+oldip="\$(cat /root/.tunnel-ip.txt)"
+localip=\$(ip a |grep brd|grep global|awk '{print \$2}'|grep /24|awk -F "/" '{print \$1}')
+if [[ \$oldip != \$remoteip ]]; then
+	ip tunnel del $tunname >/dev/null &
+	ip tunnel add $tunname mode ipip remote \${remoteip} local \${localip} ttl 64
+	ip addr add ${vip}/30 dev $tunname
+	ip link set $tunname up
+	sed -i "s/ip tunnel add $tunname mode ipip remote \${oldip} local ${localip} ttl 64/ip tunnel add $tunname mode ipip remote \${remoteip} local ${localip} ttl 64/g" /etc/rc.local
+fi
+EOF
+	fi
+	echo "${remoteip}" >/root/.tunnel-ip.txt
+	ip tunnel add $tunname mode ipip remote ${remoteip} local $localip ttl 64 # 创建IP隧道
+	ip addr add ${vip}/30 dev $tunname # 添加本机VIP
+	ip link set $tunname up # 启用隧道虚拟网卡
+	ip route add ${remotevip}/32 dev $tunname scope link src ${vip}
+	if [[ `iptables -t nat -L|grep "${remotevip}"` == "" ]]; then
+		iptables -t nat -A POSTROUTING -s ${remotevip} -j MASQUERADE
+	fi
+	if [[ `sysctl -p|grep "net.ipv4.ip_forward = 1"` == "" ]]; then
+		echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+		sysctl -p /etc/sysctl.conf
+	fi
+
 
 chmod +x /etc/rc.local
 cat > /etc/systemd/system/rc-local.service <<EOF
@@ -140,10 +141,11 @@ PublicKey = $rospublickey
 AllowedIPs = $ipduan.0/24,$allowedip1.0/24
 Endpoint = ${revip}:$wgport
 PersistentKeepalive = 25" > /etc/wireguard/$filename.conf
-	sed -i "/exit 0/i\wg-quick up $filename" /etc/rc.local
+	chmod 600 /etc/wireguard/$filename.conf
+	sed -i '$d' /etc/rc.local
+	echo -e "wg-quick up $filename\n exit 0" >> /etc/rc.local
 	wg-quick up $filename
 	vpspublickey=$(cat /etc/wireguard/publickey)
-	# vip=$(ip a|grep "scope global"|grep "/30"|awk '{print $2}'|awk -F "/" '{print $1}')
 	linstenport=$(cat /etc/wireguard/$filename.conf|grep "ListenPort"|awk '{print $3}')
 	echo "    "
 	echo -e "${green}------------------------------------------------------------${plain}"
@@ -162,7 +164,8 @@ PublicKey = $rospublickey
 AllowedIPs = $ipduan.0/24,$allowedip1.0/24
 Endpoint = ${revip}:$wgport
 PersistentKeepalive = 25" > /etc/wireguard/wg0.conf
-	sed -i '/exit 0/i\wg-quick up wg0' /etc/rc.local
+	sed -i '$d' /etc/rc.local
+	echo -e "wg-quick up wg0\n exit 0" >> /etc/rc.local
     chmod 600 /etc/wireguard/wg0.conf
 	wg-quick up wg0
 	vpspublickey=$(cat /etc/wireguard/publickey)
