3293fc38c5e1232ba1999bbbd6ec5b8c0f686a38
07031218 <minshunqiang@gmail.com>
2023-03-09 12:05:45 +0800

    Update crontab-ytdl-sub.py

--- crontab-ytdl-sub.py
+++ crontab-ytdl-sub.py
@@ -15,20 +15,34 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 
 # 查询数据库
-query = "SELECT chinesename, name, url FROM list"
+query = "SELECT chinesename, name, url, timerange FROM list"
 cursor.execute(query)
 results = cursor.fetchall()
-
+# 定义新的配置文件名格式
 # 循环遍历结果，执行任务
-for chinesename, name, url in results:
-    # 定义新的配置文件名格式
+for chinesename, name, url, timerange in results:
     config_file_name = f"subscriptions-{name}.yaml"
-    os.system(f"cp subscriptions-clasic.yaml {config_file_name}")
-    os.system(f'sed -i "s/laogao/{name}/" {config_file_name}')
-    os.system(f'sed -i "s/老高與小茉/{chinesename}/" {config_file_name}')
-    os.system(f'sed -i "s%https://www.youtube.com/@laogao/videos%{url}%" {config_file_name}')
-    os.system(f"ytdl-sub --config=/config/config-dianxixiaoge.yaml sub /config/{config_file_name}")
-    os.system(f"rm {config_file_name}")
+    if timerange == "1years":
+        os.system(f"cp subscriptions-clasic.yaml {config_file_name}")
+        os.system(f'sed -i "s/laogao/{name}/" {config_file_name}')
+        os.system(f'sed -i "s/老高與小茉/{chinesename}/" {config_file_name}')
+        os.system(f'sed -i "s/1weeks/1years/" {config_file_name}')
+        os.system(f'sed -i "s%https://www.youtube.com/@laogao/videos%{url}%" {config_file_name}')
+        os.system(f"ytdl-sub --config=/config/config-1years.yaml sub /config/{config_file_name}")
+        os.system(f"rm {config_file_name}")
+        query1 = "UPDATE `ytdl-sub`.`list` SET `timerange` = '1weeks' WHERE `name` = '%s'"%(name,)
+        try:
+            cursor.execute(query1)
+            db.commit()
+        except Exception as e:
+            print(e)
+    else:
+        os.system(f"cp subscriptions-clasic.yaml {config_file_name}")
+        os.system(f'sed -i "s/laogao/{name}/" {config_file_name}')
+        os.system(f'sed -i "s/老高與小茉/{chinesename}/" {config_file_name}')
+        os.system(f'sed -i "s%https://www.youtube.com/@laogao/videos%{url}%" {config_file_name}')
+        os.system(f"ytdl-sub --config=/config/config-dianxixiaoge.yaml sub /config/{config_file_name}")
+        os.system(f"rm {config_file_name}")
 
 # 关闭游标和数据库连接
 cursor.close()
