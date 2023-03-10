56fd96cada7a4484730f582762f729584ba271fc
07031218 <minshunqiang@gmail.com>
2023-03-10 08:52:17 +0800

    Update crontab-ytdl-sub.py

--- crontab-ytdl-sub.py
+++ crontab-ytdl-sub.py
@@ -22,6 +22,18 @@ cursor.execute(query)
 results = cursor.fetchall()
 # 循环遍历结果，执行任务
 for chinesename, name, url, timerange in results:
+    if timerange == "1years":
+        query1 = "UPDATE `ytdl-sub`.`list` SET `timerange` = '1weeks' WHERE `name` = '%s'"%(name,)
+        try:
+            db.ping(reconnect=True)
+            cursor.execute(query1)
+            db.commit()
+        except Exception as e:
+            print(e)
+cursor.close()
+db.close()
+# 循环遍历结果，执行任务
+for chinesename, name, url, timerange in results:
     # 定义新的配置文件名格式
     config_file_name = f"subscriptions-{name}.yaml"
     if timerange == "1years":
@@ -32,13 +44,7 @@ for chinesename, name, url, timerange in results:
         os.system(f'sed -i "s%https://www.youtube.com/@xxx/videos%{url}%" {config_file_name}')
         os.system(f"ytdl-sub --config=/config/config-1years.yaml sub /config/{config_file_name}")
         os.system(f"rm {config_file_name}")
-        query1 = "UPDATE `ytdl-sub`.`list` SET `timerange` = '1weeks' WHERE `name` = '%s'"%(name,)
-        try:
-            db.ping(reconnect=True)
-            cursor.execute(query1)
-            db.commit()
-        except Exception as e:
-            print(e)
+
     else:
         os.system(f"cp subscriptions-clasic.yaml {config_file_name}")
         os.system(f'sed -i "s/laogao/{name}/" {config_file_name}')
@@ -48,7 +54,5 @@ for chinesename, name, url, timerange in results:
         os.system(f"rm {config_file_name}")
 
 # 关闭游标和数据库连接
-cursor.close()
-db.close()
 os.system(f"gclone copy /tv_shows/ SA:影视剧/油管专辑 --drive-server-side-across-configs --transfers=8 -P")
 os.system(f"cd /tv_shows && rm ./* -rf")
