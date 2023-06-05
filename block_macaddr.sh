8ca8b52f83945bc6ca777d782e87ff19a174c0d9
07031218 <minshunqiang@gmail.com>
2023-06-05 20:28:34 +0800

    Update block_macaddr.sh

--- block_macaddr.sh
+++ block_macaddr.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 # iptables通过设备MAC地址来禁止对应设备连接互联网(治熊孩子专用)
 # 根据自己设备的MAC地址进行替换操作即可
-# 如果您使用的是 LuCI Web 界面，则可以使用 System 模块来设置开机运行的脚本。
-# 登录到 LuCI 界面。
-# 单击“System”选项卡。
-# 单击“Startup”选项卡。
-# 在“Local Startup”文本框中输入您要运行的命令或脚本。
+# 如果您使用的是OpenWRT软路由，在LuCI Web界面，通过 系统 - 启动项 - 本地启动脚本 模块来设置开机执行。
 # Powerby 翔翎
 if [[ `iptables -L FORWARD -n|grep 'MAC48:98:ca:05:16:27'` == "" ]]; then
 	iptables -I FORWARD -m mac --mac-source A8:82:00:C9:C0:E0 -j DROP # 客厅电视有线
